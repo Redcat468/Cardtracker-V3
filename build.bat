3d0e20840aa0bab9457643986ebadffef53f3f03
Redcat <77931956+Redcat468@users.noreply.github.com>
2025-01-23 14:09:04 +0100

    added favicon

--- build.bat
+++ build.bat
@@ -6,13 +6,14 @@ pyinstaller --onefile --name "cardtracker" ^
 --icon "static\images\logo.ico" ^
 --add-data "templates;templates" ^
 --add-data "static;static" ^
---add-data "instance;instance" ^
+--add-data "database.py;." ^
 --add-data "models.py;." ^
 --add-data "routes.py;." ^
---add-data "database.py;." ^
 --hidden-import "flask_sqlalchemy" ^
 --hidden-import "flask_login" ^
 --hidden-import "sqlalchemy.ext.declarative" ^
+--hidden-import "database" ^
+--noconsole ^
 --clean ^
 app.py
 
